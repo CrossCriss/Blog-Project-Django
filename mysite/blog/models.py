0e91c3466357bfd2d4e1c1c148e11a4b80e9d6c4
Viacheslav Herasimyuk <herasimyukviacheslav@gmail.com>
2023-02-16 14:41:58 +0200

    Start app Blog and create new model for posts

---
+++ mysite/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+# Создание модели публикации
+class Post(models.Model): 
+    title = models.CharField(max_lenght=250)
+    slug = models.SlugField(max_leght=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
