4b17b0c746eddf9f3d0269eec1a7818f20ce6f43
Viacheslav Herasimyuk <herasimyukviacheslav@gmail.com>
2023-02-19 17:34:06 +0200

    Added EmailPostForm

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
 from django.views.generic import ListView
-
-
-# def post_list(request):
-#     post_list = Post.published.all()
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         post = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,
-#                   'blog/post/list.html',
-#                   {'posts': posts})
 
 
 class PostListView(ListView):
@@ -37,3 +22,21 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+
+        else:
+            form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
