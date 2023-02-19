dcac928d20c069a65dc7cad2028c74f458e73570
Viacheslav Herasimyuk <herasimyukviacheslav@gmail.com>
2023-02-19 18:53:10 +0200

    Added share with email feature

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -111,3 +111,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'politikus2@gmail.com'
+EMAIL_HOST_PASSWORD = 'fbimtkllgedwjqgw'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
